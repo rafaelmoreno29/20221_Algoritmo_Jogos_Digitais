8d094a63b97b7e2b670f1196434f88ee22c2838c
Rafael Moreno <rafaelmorenosax@gmail.com>
2022-04-27 11:07:27 -0300

    vetor

---
+++ ExemploPersonagemDano/ExemploPersonagemDano/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPersonagemDano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int classe, dano, vida;
+
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Escolha: 1 - Paladino ou 2 - Arqueiro");
+            classe = Convert.ToInt32(Console.ReadLine());
+
+            if (classe == 1)
+                vida = 20;
+            else
+                vida = 10;
+
+            while (vida > 0)
+            {
+                Random random = new Random();
+                int escapou = random.Next(1,3);
+
+                if (escapou == 1)
+                {
+                    Console.WriteLine("Você escapou do ataque!!!");
+                }
+                else
+                {
+                    dano = random.Next(1, 6);
+                    Console.WriteLine("Você sofreu um dano de: " + dano);
+                    vida = vida - dano;
+                    Console.WriteLine("Restou " + vida + " vida");
+                }
+
+                Console.WriteLine("Aperte uma tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ExemploVetor/ExemploVetor/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[6];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("Digite o " + (i+1) + "º número");
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 5;i >= 0; i--)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+
+
+
+
+            //Criei um vetor com 10 posições
+          //  int[] numeros = new int[10];
+
+            //coloquei valor 1 na posição 0
+            // numeros[0] = 1;
+            //coloquei valor 2 na posição 1
+            // numeros[1] = 2;
+
+            //gerar números randomicos para o vetor
+            // Random random = new Random();
+            //for(int i = 0; i < numeros.Length; i++)
+            //{
+            //    numeros[i] = random.Next(1,101);
+            //}
+
+            //usuario digitar o valores do vetor
+           // for (int i = 0; i < numeros.Length; i++)
+           // {
+            //    Console.WriteLine("Digite o valor da posição " + i);
+           //     numeros[i] = Convert.ToInt32(Console.ReadLine());
+           // }
+
+            //Mostrei na tela todos os valores do vetor
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+           //     Console.WriteLine("Posição " + i + ": " + numeros[i]);
+           // }
+            
+
+          //  Console.ReadKey();
+        }
+    }
+}
